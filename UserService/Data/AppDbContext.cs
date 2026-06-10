4d8cf8100da99c2dd368c412e7b29836e28f0345
palchaplot <chaplotpal@gmail.com>
2026-06-10 09:05:54 +0530

    Initial backend setup with UserService and EF Core

---
+++ UserService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+
+		}
+		public DbSet<User> Users { get; set; }
+	}
+}
