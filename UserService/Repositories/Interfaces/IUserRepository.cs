d2f8d854c4889d2db1911171105a4261a43d5bc0
palchaplot <chaplotpal@gmail.com>
2026-06-11 10:15:45 +0530

    Implemented User Repository Pattern

---
+++ UserService/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using UserService.Models;
+using Microsoft.EntityFrameworkCore;
+namespace UserService.Repositories.Interfaces;
+
+
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+
+    Task<User?> GetByIdAsync(int id);
+
+    Task AddUserAsync(User user);
+
+    Task UpdateUserAsync(User user);
+
+    Task DeleteUserAsync(User user);
+
+
+}
---
+++ UserService/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserService.Data;
+using UserService.Models;
+using UserService.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace UserService.Repositories
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.UserId == id);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteUserAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+        
+
+        
+    }
+}
