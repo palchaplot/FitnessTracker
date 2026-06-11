e7959d6b37ac6d305bcf5fa427c9668c8c4d2aa0
palchaplot <chaplotpal@gmail.com>
2026-06-11 11:01:15 +0530

    Added DTO

--- UserService/DTO/Login.cs
+++ UserService/DTO/Login.cs
@@ -2,5 +2,7 @@
 {
     public class Login
     {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
--- UserService/DTO/Register.cs
+++ UserService/DTO/Register.cs
@@ -2,5 +2,11 @@
 {
     public class Register
     {
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
     }
 }
